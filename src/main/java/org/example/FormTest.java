f4030b87dd2fbd97f408f1ffe550cef851ca578b
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-01-15 18:06:46 +0100

    Correct selector for assert found

--- src/main/java/org/example/FormTest.java
+++ src/main/java/org/example/FormTest.java
@@ -8,10 +8,12 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.regex.Pattern;
+import static org.testng.AssertJUnit.assertEquals;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-
+/***
+ * Class comment
+ *
+ */
 public class FormTest {
     Playwright playwright;
     Browser browser;
@@ -21,33 +23,45 @@ public class FormTest {
     public void setup() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(100));
-        page = browser.newPage();;
+        page = browser.newPage();
 
     }
 
+    /***
+     * Testing validationMessage when email is not valid
+     */
     @Test
-    public void CodeGenDemo() {
-
+    public void validateFormMessage() {
         page.navigate("https://selectorshub.com/xpath-practice-page/");
+        // call customerTest1
+        fillFormData("dragan.test.se", "123456test", "testing ab", 123456789L);
+        // Click text=Submit
+        page.click("text=Submit");
+
+        String validationMessage = (String) page.evaluate("document.querySelector('#userId').validationMessage");
+        assertEquals("Please include an '@' in the email address. 'dragan.test.se' is missing an '@'.", validationMessage);
+
+    }
+
+    public void fillFormData(String email, String password, String company, long mobileNumber){
         // Click [placeholder="Enter email"]
         page.click("[placeholder=\"Enter email\"]");
         // Fill [placeholder="Enter email"]
-        page.fill("[placeholder=\"Enter email\"]", "dragan@test.se");
+        page.fill("[placeholder=\"Enter email\"]", email);
         // Click [placeholder="Enter Password"]
         page.click("[placeholder=\"Enter Password\"]");
         // Fill [placeholder="Enter Password"]
-        page.fill("[placeholder=\"Enter Password\"]", "123456");
+        page.fill("[placeholder=\"Enter Password\"]", password);
         // Click [placeholder="Enter your company"]
         page.click("[placeholder=\"Enter your company\"]");
         // Fill [placeholder="Enter your company"]
-        page.fill("[placeholder=\"Enter your company\"]", "test ab");
-        // Click text=Submit
-        page.waitForNavigation(() -> {
-            page.click("text=Submit");
-        });
-        assertThat(page).hasURL(Pattern.compile(".*https://selectorshub.com/xpath-practice-page/"));
-
+        page.fill("[placeholder=\"Enter your company\"]", company);
+        // Click [placeholder="Enter your mobile number"]
+        page.click("[placeholder=\"Enter your mobile number\"]");
+        // Fill [placeholder="Enter your mobile number"]
+        page.fill("[placeholder=\"Enter your mobile number\"]", String.valueOf(mobileNumber));
     }
+
 
     @AfterTest
     public void tearDown(){
