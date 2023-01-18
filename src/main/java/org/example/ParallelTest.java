cb7be49049c3461fb5951780eef4e1345909a67d
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-01-18 19:35:07 +0100

    Added comment

--- src/main/java/org/example/ParallelTest.java
+++ src/main/java/org/example/ParallelTest.java
@@ -5,13 +5,22 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+/***
+ * This code uses the Playwright tool to run a test in three
+ * different browsers (Chrome, Firefox, and Webkit) in parallel using threads.
+ * The test navigates to a website, fills in login credentials, and logs out.
+ * The main method creates three threads, one for each browser, and starts them.
+ * The getBrowser method takes in a Playwright object and a browser name,
+ * and returns the corresponding browser type. The run method is overridden
+ * to include the logic for the test. The test is executed in parallel in
+ * all three browsers, and the browser and page objects are closed after the test is completed.
+ */
 public class ParallelTest extends Thread{
     String browserName;
     public ParallelTest(String browserName) {
         this.browserName = browserName;
     }
 
-    // running test parallel in three browsers using threads
     public void run(){
         System.out.println("Thread running...");
         Playwright playwright = Playwright.create();
